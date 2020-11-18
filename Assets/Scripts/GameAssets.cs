e48e5c04cda6cb73aeb1e17b11e584342e6617a6
Ryan Gauger <rgauger.dev@gmail.com>
2020-11-18 22:07:24 +1000

    Init Snake

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+
+    public static GameAssets Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+  
+
+    public Sprite snakeHeadSprite;
+    public Sprite foodSprite;
+}
